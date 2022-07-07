c612870cddd677ddf7ad92b5131c7ae82a601a7e
yong <yong.@qq.com>
2022-07-07 15:36:37 +0800

    Third Commit for test

---
+++ Student_crud/src/test/java/SelectTest/git_test.java
@@ -0,0 +1,14 @@
+package SelectTest;
+
+/**
+ * @author
+ * @description:
+ * @create 2022/7/7-15:31
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git by idea");
+        System.out.println("hello git by idea Second");
+        System.out.println("hello git by idea Thrid");
+    }
+}
